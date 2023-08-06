1af20fdb3c777ee1997e4b95259604686053438c
MaVCor13 <134764216+MaVCor13@users.noreply.github.com>
2023-08-05 22:48:31 -0300

    script

--- Practico1/script.js
+++ Practico1/script.js
@@ -60,4 +60,52 @@ document.getElementById("calculate").addEventListener("click", function () {
 
     document.getElementById("result").textContent = "Resultado: " + result;
 });
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + b;
+}
+
+// Función para restar dos números
+function restar(a, b) {
+    return a - b;
+}
+
+// Función para multiplicar dos números
+function multiplicar(a, b) {
+    return a * b;
+}
+
+// Función para dividir dos números
+function dividir(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return "Error: No se puede dividir por cero";
+    }
+}
+
+// Event listener para el botón "Calcular"
+document.getElementById("calculate").addEventListener("click", function () {
+    var operand1 = parseFloat(document.getElementById("operand1").value);
+    var operand2 = parseFloat(document.getElementById("operand2").value);
+    var operator = document.getElementById("operator").value;
+    var result = 0;
+
+    switch (operator) {
+        case "sum":
+            result = sumar(operand1, operand2);
+            break;
+        case "subtract":
+            result = restar(operand1, operand2);
+            break;
+        case "multiply":
+            result = multiplicar(operand1, operand2);
+            break;
+        case "divide":
+            result = dividir(operand1, operand2);
+            break;
+    }
+
+    document.getElementById("result").textContent = "Resultado: " + result;
+});
 
